201a665c8bc080bf7e1a611dbe48a9e31d17e54f
Richard Bao <richardbao419@gmail.com>
2020-05-16 14:09:15 -0700

    update

--- preprocess/parser.py
+++ preprocess/parser.py
@@ -3,25 +3,27 @@ import ast
 import numpy as np
 import matplotlib.pyplot as plt
 
+class AppURLopener(urllib.request.FancyURLopener):
+    version = "Mozilla/5.0"
 
 def get_html(url):
 	# with urllib.request.urlopen(url) as response:
 	# 	html = response.read().decode('ascii')
 	# 	return html
-	req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-	with urllib.request.urlopen(req) as response:
-		html = response.read().decode('ascii')
-		return html
 
-class AppURLopener(urllib.request.FancyURLopener):
-    version = "Mozilla/5.0"
-
-
-if __name__ == '__main__':
-	url = "https://uselectionatlas.org/RESULTS/national.php?f=1&year=2012&off=0&elect=0"
-	# leip = get_html(url)
+	# req = urllib.request.Request(url, headers={'User-Agent': 'Mozilla/5.0'})
+	# with urllib.request.urlopen(req) as response:
+	# 	html = response.read().decode('ascii')
+	# 	return html
 
 	opener = AppURLopener()
 	with opener.open(url) as response:
 		html = response.read().decode('ascii')
-		print(html)
+		return html
+
+
+if __name__ == '__main__':
+	year = 2012
+	url = f"https://uselectionatlas.org/RESULTS/national.php?year={year}"
+	leip = get_html(url)
+	print(leip)
