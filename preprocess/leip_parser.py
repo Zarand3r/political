2ee332c1dcf18f9aa0a84bbfe39025f44e270932
Richard Bao <richardbao419@gmail.com>
2020-05-23 11:49:17 -0700

    update

--- preprocess/leip_parser.py
+++ preprocess/leip_parser.py
@@ -1,11 +1,18 @@
 import urllib.request
 from bs4 import BeautifulSoup
-import ast
 import numpy as np
+import pandas as pd
+from itertools import repeat
+from multiprocessing import Pool
 import matplotlib.pyplot as plt
+import ast
+import csv
+import os
+import re
+numeric = re.compile(r'[^\d.]+')
 
 class AppURLopener(urllib.request.FancyURLopener):
-    version = "Mozilla/5.0"
+	version = "Mozilla/5.0"
 
 def get_html(url):
 	# with urllib.request.urlopen(url) as response:
@@ -19,62 +26,135 @@ def get_html(url):
 
 	opener = AppURLopener()
 	with opener.open(url) as response:
-		html = response.read().decode()
+		html = response.read().decode("latin1")
 		return html
 
-# def fetch_data:
-# 	return
+def write_to_csv(results, output_file):
+	with open(output_file, 'w') as submission_file:
+		writer = csv.writer(submission_file, delimiter=',')
+		writer.writerows(results)
 
-def get_links(html):
+def get_links(html, filter=None):
 	url_list = []
 	soup = BeautifulSoup(html, "html.parser")
 	for link in soup.findAll('area'):
-	    url_list.append(link.get('href'))
+		url = link.get('href')
+		if filter=="state":
+			if str(url)[0:5] != filter:
+				continue 
+		url_list.append(url)
 	return url_list
 
 def get_votes(html):
-	print(html)
 	soup = BeautifulSoup(html, "html.parser")
 	table = soup.find_all('tbody')[0]
-	print(table)
+	candidates = []
+	votes = []
 	for row in table.find_all('tr'):
-		print(row)
-	return row
+		cell_list = row.find_all("td")
+		candidates.append(cell_list[1].text)
+		votes.append(numeric.sub('', str(cell_list[-1].text)))
+	return list(zip(candidates, votes))
 
-def load_general_election():
+def get_county_fips(url):
+	fips = url.split("=")
+	fips = fips[2]
+	fips = numeric.sub('',fips)
+	return fips
+
+def load_general_election(output_file="../data/general.csv"):
 	leip_site = "https://uselectionatlas.org/RESULTS/"
 	# years_list = np.arange(1940, 2020, step=4)
-	years_list = np.arange(1980, 2020, step=4)
+	years_list = np.arange(2000, 2020, step=4)
 	data = []
 
 	# Sliced the lists for testing purposes, so we dont need to go through everything
-	for year in years_list[0:1]:
+	for year in years_list:
 		us_url = leip_site+ f"national.php?year={year}"
 		us_html = get_html(us_url)
-		states_url_list = get_links(us_html)
-		for state_url in states_url_list[0:1]:
+		states_url_list = get_links(us_html,filter="state")
+		for state_url in states_url_list:
 			state_url = leip_site + state_url
 			state_html = get_html(state_url)
 			counties_url_list = get_links(state_html)
-			for county_url in counties_url_list[0:1]:
+			for county_url in counties_url_list:
 				county_url = leip_site + county_url
 				county_html = get_html(county_url)
-				votes = get_votes(county_html)
-				# This currently just prints out the rows in the county html corresponding canddiates and votes
-				# Next step is to append [county fips, year, candidate, vote] to the data variable at this bottom loop
-				# Then, we can return the data list as a list of lists, and write it into a csv
+				fips = get_county_fips(county_url)
+				election_results = get_votes(county_html)
+				for result in election_results:
+					data.append([year, fips, result[0], result[1]])
+					# append the fips code too. 
+					# issue: for early dates, county urls dont exist and instead we go need to go to the state level county datagraph and parse results from the table there
+					# update: my ip address got blacklisted for too many queires. going anywhere on the site directs me to Forbidden, when im on browser. Parser still seems to work.
+					# solution: use vpn?
+	write_to_csv(data, output_file=output_file)
+	return data
 
-def load_primary_election():
-	# replace the fips at the end for a quick way to get votes for each county
-	#https://uselectionatlas.org/RESULTS/statesub.php?year=2012&fips=36061
+def load_primary_election(output_file="../data/primary.csv"):
+	fips_key = pd.read_csv("fips_key.csv", encoding="latin-1")
+	fips_list = fips_key["FIPS"]
+	total = len(fips_list)
+	years_list = np.arange(2000, 2020, step=4)
 
-	# Alternatively, start at this url and do the same nested for loop as for the general election
-	# Beware that getting the urls at each level will take an additional step
-	# The href are have url parameters formatted with semicolon, wont work as links, need to reformat
-	#https://uselectionatlas.org/RESULTS/national.php?f=1&year=2020&elect=1
+	democratic_data = []
+	for year in years_list:
+		for index, fips in enumerate(fips_list): 
+			print(f"{index+1} / {total}")
+			county_url = f"https://uselectionatlas.org/RESULTS/statesub.php?year={year}&fips={fips}&elect=1&evt=P"
+			print(county_url)
+			county_html = get_html(county_url)
+			election_results = get_votes(county_html)
+			for result in election_results:
+				democratic_data.append([year, fips, result[0], result[1]])
+	output_file = output_file[:-4]+"_democratic.csv"
+	write_to_csv(democratic_data, output_file=output_file)
 
+	republican_data = []
+	for year in years_list:
+		for index, fips in enumerate(fips_list): 
+			print(f"{index+1} / {total}")
+			county_url = f"https://uselectionatlas.org/RESULTS/statesub.php?year={year}&fips={fips}&elect=2&evt=P"
+			print(county_url)
+			county_html = get_html(county_url)
+			election_results = get_votes(county_html)
+			for result in election_results:
+				republican_data.append([year, fips, result[0], result[1]])
+	output_file = output_file[:-4]+"_republican.csv"
+	write_to_csv(republican_data, output_file=output_file)
 
+	return (democratic_data, republican_data)
+
+# def load_single_primary_election(year, fips_list):
+# 	total = len(fips_list)
+# 	data = []
+# 	for index, fips in enumerate(fips_list): 
+# 		print(f"{index+1} / {total}")
+# 		county_url = f"https://uselectionatlas.org/RESULTS/statesub.php?year={year}&fips={fips}"
+# 		print(county_url)
+# 		county_html = get_html(county_url)
+# 		election_results = get_votes(county_html)
+# 		for result in election_results:
+# 			data.append([year, fips, result[0], result[1]])
+# 	return data
+
+# def multi_load_primary_election(output_file="../data/primary.csv"):
+# 	fips_key = pd.read_csv("fips_key.csv", encoding="latin-1")
+# 	fips_list = fips_key["FIPS"][0:10]
+# 	years_list = np.arange(2000, 2020, step=4)
+# 	primary_results = []
+
+# 	pool = Pool(os.cpu_count()) ## According to TA this will saturate more cores in the hpc?
+# 	results = pool.starmap(load_single_primary_election, zip(years_list, repeat(fips_list)))
+	
+# 	for result in results:
+# 		primary_results = primary_results + list(result)
+
+# 	write_to_csv(primary_results, output_file=output_file)
+# 	return primary_results
 
 
 if __name__ == '__main__':
-	load_data()
+	# load_general_election()
+	load_primary_election()
+	# multi_load_primary_election()
